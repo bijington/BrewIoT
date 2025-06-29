ce952f3a9b25931eecbe460e778936d65ec70bbd
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2025-06-29 21:24:02 +0100

    Add auto refresh for device readings

--- client/BrewIoT.Client/Devices/ViewModels/DevicePageViewModel.cs
+++ client/BrewIoT.Client/Devices/ViewModels/DevicePageViewModel.cs
@@ -7,6 +7,9 @@ namespace BrewIoT.Client.Devices.ViewModels;
 public partial class DevicePageViewModel : ObservableObject, IQueryAttributable
 {
     private readonly IDeviceApiService deviceApiService;
+    private readonly IDispatcherTimer dispatcherTimer;
+    
+    private Device? device;
 
     [ObservableProperty]
     private string name = string.Empty;
@@ -20,19 +23,53 @@ public partial class DevicePageViewModel : ObservableObject, IQueryAttributable
     [ObservableProperty] 
     private string noReadingsMessage = "No readings available.";
 
-    public DevicePageViewModel(IDeviceApiService deviceApiService)
+    public DevicePageViewModel(IDeviceApiService deviceApiService, IDispatcher dispatcher)
     {
         this.deviceApiService = deviceApiService;
+
+        this.dispatcherTimer = dispatcher.CreateTimer();
+        this.dispatcherTimer.IsRepeating = false;
+        this.dispatcherTimer.Interval = TimeSpan.FromSeconds(30);
+        this.dispatcherTimer.Tick += DispatcherTimerOnTick;
     }
 
     public async void ApplyQueryAttributes(IDictionary<string, object> query)
     {
         try
         {
-            var device = (Device)query["Device"];
+            device = (Device)query["Device"];
             
             Name = device.Name;
             
+            await LoadReadings();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            NoReadingsMessage = e.Message;
+        }
+    }
+
+    public void OnNavigatingFrom()
+    {
+        this.dispatcherTimer.Stop();
+        this.dispatcherTimer.Tick -= DispatcherTimerOnTick;
+    }
+    
+    private async void DispatcherTimerOnTick(object? sender, EventArgs e)
+    {
+        await LoadReadings();
+    }
+
+    private async Task LoadReadings()
+    {
+        if (device is null)
+        {
+            return;
+        }
+        
+        try
+        {
             var deviceReadings = await deviceApiService.GetDeviceReadings(device.Id);
             
             Readings = new ObservableCollection<DeviceReading>(deviceReadings.OrderByDescending(x => x.Timestamp));
@@ -45,5 +82,7 @@ public partial class DevicePageViewModel : ObservableObject, IQueryAttributable
             NoReadingsMessage = e.Message;
             throw;
         }
+        
+        this.dispatcherTimer.Start();
     }
 }
